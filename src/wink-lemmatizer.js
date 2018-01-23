c234ceac40d93e9c5426c10289c779e72b54fb77
Sanjaya <sanjaya@graype.in>
2018-01-23 12:45:58 +0530

    feat(wink-lemmatizer): complete wrapper and its test cases

---
+++ src/wink-lemmatizer.js
@@ -0,0 +1,30 @@
+//     wink-lemmatizer
+//     English lemmatizer
+//
+//     Copyright (C) 2017-18  GRAYPE Systems Private Limited
+//
+//     This file is part of “wink-lemmatizer”.
+//
+//     “wink-lemmatizer” is free software: you can redistribute
+//     it and/or modify it under the terms of the GNU Affero
+//     General Public License as published by the Free
+//     Software Foundation, version 3 of the License.
+//
+//     “wink-lemmatizer” is distributed in the hope that it will
+//     be useful, but WITHOUT ANY WARRANTY; without even
+//     the implied warranty of MERCHANTABILITY or FITNESS
+//     FOR A PARTICULAR PURPOSE.  See the GNU Affero General
+//     Public License for more details.
+//
+//     You should have received a copy of the GNU Affero
+//     General Public License along with “wink-lemmatizer”.
+//     If not, see <http://www.gnu.org/licenses/>.
+
+var wl = Object.create( null );
+
+// noun lemmatizer
+wl.lemmatizeNoun = require( './lemmatize-nnx.js' );
+// verb lemmatizer
+wl.lemmatizeVerb = require( './lemmatize-vbx.js' );
+
+module.exports = wl;
---
+++ test/wink-lemmatizer-specs.js
@@ -0,0 +1,44 @@
+//     wink-lemmatizer
+//     English lemmatizer
+//
+//     Copyright (C) 2017-18  GRAYPE Systems Private Limited
+//
+//     This file is part of “wink-lemmatizer”.
+//
+//     “wink-lemmatizer” is free software: you can redistribute
+//     it and/or modify it under the terms of the GNU Affero
+//     General Public License as published by the Free
+//     Software Foundation, version 3 of the License.
+//
+//     “wink-lemmatizer” is distributed in the hope that it will
+//     be useful, but WITHOUT ANY WARRANTY; without even
+//     the implied warranty of MERCHANTABILITY or FITNESS
+//     FOR A PARTICULAR PURPOSE.  See the GNU Affero General
+//     Public License for more details.
+//
+//     You should have received a copy of the GNU Affero
+//     General Public License along with “wink-lemmatizer”.
+//     If not, see <http://www.gnu.org/licenses/>.
+
+//
+/* eslint-disable no-console */
+
+var chai = require( 'chai' );
+var mocha = require( 'mocha' );
+var lemmatizer = require( '../src/wink-lemmatizer.js' );
+
+var expect = chai.expect;
+var describe = mocha.describe;
+var it = mocha.it;
+
+describe( 'lemmatizeVerb test', function () {
+  it( 'lemmatize tries must give try', function () {
+      expect( lemmatizer.lemmatizeVerb( 'tries' ) ).to.deep.equal( 'try' );
+  } );
+} );
+
+describe( 'lemmatizeNoun test', function () {
+  it( 'lemmatize shoes must give shoe', function () {
+      expect( lemmatizer.lemmatizeNoun( 'shoes' ) ).to.deep.equal( 'shoe' );
+  } );
+} );
